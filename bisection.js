5a9d7ff9786dd7d804f741da7f3a8bd1540dfc83
Test <101502649+susamobus@users.noreply.github.com>
2022-10-01 09:23:02 +0800

    fix

--- bisection.js
+++ bisection.js
@@ -6,7 +6,7 @@ var y = 2
 var a = ExpantaNum.div(y,2)
 
 while (ExpantaNum.gt(ExpantaNum.arrow(x,arrow,y),y)) {
-    steps += 1
+    steps = steps + 1
     a = ExpantaNum.div(a,2)
 }
 
@@ -14,7 +14,7 @@ var b = ExpantaNum.mul(a,2)
 var c = a
 while (ExpantaNum.neq(ExpantaNum.div((ExpantaNum.add(a,b)),2),c)) {
     c = ExpantaNum.div((ExpantaNum.add(a,b)),2)
-    steps += 1
+    steps = steps + 1
     if (ExpantaNum.lt(ExpantaNum.arrow(c,arrow,x),y)) {
         a = c
     } else {
